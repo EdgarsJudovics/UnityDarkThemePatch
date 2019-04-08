53a8997087c261ecee35a3522a7c9166e3951d36
Martin Tracey <matracey@outlook.ie>
2019-04-08 12:38:56 +0100

    Updated UnityBinary to support ExecutableVersionString.

--- UnityDarkThemePatch/Models/UnityBinary.cs
+++ UnityDarkThemePatch/Models/UnityBinary.cs
@@ -8,12 +8,43 @@ namespace UnityDarkThemePatch.Models
 {
     public class UnityBinary
     {
-        public UnityBinary(string filePath)
+        /// <summary>
+        /// The path to Unity.exe.
+        /// </summary>
+        public string ExecutablePath { get; set; }
+
+        /// <summary>
+        /// The Unity version.
+        /// </summary>
+        public UnityBinaryVersion ExecutableVersion { get; set; }
+
+        /// <summary>
+        /// The Unity version string as read from the executable path.
+        /// </summary>
+        public string ExecutableVersionString { get; set; }
+
+        public UnityBinary(string filePath, string versionString = "")
         {
             if (File.Exists(filePath))
             {
                 ExecutablePath = filePath;
                 ExecutableVersion = GetUnityBinaryVersion(filePath);
+
+                if (!string.IsNullOrWhiteSpace(versionString))
+                {
+                    ExecutableVersionString = versionString;
+                }
+                else if (string.IsNullOrWhiteSpace(ExecutableVersionString))
+                {
+                    var descriptionAttr = ExecutableVersion.GetType().GetField(ExecutableVersion.ToString()).GetCustomAttribute<DescriptionAttribute>();
+                    if (descriptionAttr != null)
+                    {
+                        ExecutableVersionString = descriptionAttr.Description;
+                    } else
+                    {
+                        ExecutableVersionString = "";
+                    }
+                }
             }
         }
 
@@ -21,6 +52,7 @@ namespace UnityDarkThemePatch.Models
         {
             var splitPath = filePath.Split('\\');
             var versionString = splitPath[splitPath.Length - 3];
+            ExecutableVersionString = versionString;
 
             var versionOptions = Enum.GetValues(typeof(UnityBinaryVersion)).Cast<UnityBinaryVersion>();
             versionOptions.DefaultIfEmpty(UnityBinaryVersion.UNKNOWN);
@@ -37,8 +69,5 @@ namespace UnityDarkThemePatch.Models
                 return filePath.Contains(v.ToString());
             });
         }
-
-        public string ExecutablePath { get; set; }
-        public UnityBinaryVersion ExecutableVersion { get; set; }
     }
 }
